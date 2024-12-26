14b94b8dd76e73d2d7a867c42912322abd639a6f
David Ortiz <david.ortiz.p@gmail.com>
2024-12-26 12:03:29 -0500

    added searchPokemon service

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,7 +1,8 @@
-const BASE_URL = "https://poke-collection-lite-production.up.railway.app";
+const POKECOLLECTION_BASE_URL = "https://poke-collection-lite-production.up.railway.app";
+const POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 export const getFavorites = async (username) => {
-  const url = `${BASE_URL}/api/${username}/favorites`
+  const url = `${POKECOLLECTION_BASE_URL}/api/${username}/favorites`
   const response = await fetch(url);
   console.log("response ", response);
   if (response.ok) {
@@ -12,3 +13,17 @@ export const getFavorites = async (username) => {
     console.log(error.message || "an error ocurred");
   }
 };
+
+export const searchPokemon = async (name) => {
+  const url = `${POKEAPI_BASE_URL}${name}`
+  const response = await fetch(url);
+  console.log("response ", response);
+  if (response.ok) {
+    const data = await response.json();
+    console.log("data ", data);
+  } else {
+    const error = await response.json();
+    console.log(error.message || "an error ocurred");
+  }
+
+}
